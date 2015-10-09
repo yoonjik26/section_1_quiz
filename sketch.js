b03f1313295555cfb0c35ede513f8ca32d3e8069
Justin Bakse <jbakse@brandnewschool.com>
2015-10-09 13:02:31 -0400

    initial import

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+	// create a place to draw
+	createCanvas(640, 360);
+}
+
+function draw() {
+	// clear the background
+	background(50, 50, 50);
+
+	// set the drawing style
+	fill(100, 100, 100);
+	stroke(255, 255, 255);
+
+	// draw an ellipse
+	ellipse(320, 180, 100, 100);
+}
